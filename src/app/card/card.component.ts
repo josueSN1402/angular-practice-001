9f904efca867f69e6f0b27c76417ce2ee9ed3ac5
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-04-09 00:14:59 -0500

    Cambias para alinear la altura de las card (fallo)

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pa-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input() title = '';
+  @Input() content = '';
 
+  constructor() {}
 }
